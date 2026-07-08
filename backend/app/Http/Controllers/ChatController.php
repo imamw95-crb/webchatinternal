c58c61033c4293c5e520ede3789b74de89b89380
imm95 <im95@gdrivepro.com>
2026-07-08 15:59:45 +0700

    Update ChatController and add DOCUMENTATION.md

--- backend/app/Http/Controllers/ChatController.php
+++ backend/app/Http/Controllers/ChatController.php
@@ -9,7 +9,6 @@ use App\Models\Message;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
 class ChatController extends Controller
